66fba17eeca29950be7bec9bd1a46f71e790c557
passaelen <pas.saelen@gmail.com>
2026-05-07 11:57:22 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,17 +1,21 @@
 const DATA = {
   portfolio: {
-    or_suisse_g: 201.52,
-    or_belgique_g: 0,
-    argent_g: 0,
+    or_suisse_g: 338.68,
+    or_belgique_g: 130.62,
+    argent_g: 6000,
 
-    or_suisse_cost: 9125,
-    or_belgique_cost: 0,
-    argent_cost: 0
+    or_suisse_cost: 19722.90,
+    or_belgique_cost: 8467.05,
+    argent_cost: 3379.88
   },
 
   etf: {
-    monde: { parts: 1, buy: 152.86 },
-    seniors: { parts: 6, buy: 448.63 },
+    monde: { parts: 8, buy: 1232.64 },
+    seniors: { parts: 13, buy: 1048.57 },
     juniors: { parts: 0, buy: 0 }
   }
 };
+
+// ⚠️ IMPORTANT
+// Les parts ETF sont gérées ici (source de vérité)
+// Le Google Sheet doit être mis à jour pour rester cohérent avec l’historique
