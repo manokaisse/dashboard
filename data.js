5a3f595186a1ea23cdd5b77693a140eb81b8c6cc
passaelen <pas.saelen@gmail.com>
2026-08-05 11:26:57 +0200

    Update data.js

--- data.js
+++ data.js
@@ -11,7 +11,7 @@ const DATA = {
 
   etf: {
     argent: { parts: 5, buy: 248.36 },
-    monde: { parts: 3, buy: 484.32 },
+    monde: { parts: 4, buy: 653.43 },
     seniors: { parts: 14, buy: 1028.89 },
     juniors: { parts: 0, buy: 0 },
     petrole: { parts: 0, buy: 0},
