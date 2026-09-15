6a4b6a0f6b62c721456028b92a2f2492d51cc2cd
passaelen <pas.saelen@gmail.com>
2026-09-15 07:35:06 +0200

    Update achats.js

--- achats.js
+++ achats.js
@@ -1,4 +1,5 @@
 const achats = [
+{date: "03/09/2026", type: "ETF_MONDE", montant: 171.38, parts: 1, label: "1 ETF Monde"},  
 {date: "03/08/2026", type: "ETF_MONDE", montant: 169.11, parts: 1, label: "1 ETF Monde"},
 {date: "17/07/2026", type: "ETF_SENIOR", montant: 212.15, parts: 3, label: "3 ETF SENIORS"},  
 {date: "16/07/2026", type: "ETF_SENIOR", montant: 219.66, parts: 3, label: "3 ETF SENIORS"},
